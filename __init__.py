517372f3f8d836f44095a88a6e68f21ea9ab1eb1
relhamdi <elhamdi.rayan@gmail.com>
2026-05-21 00:16:38 +0200

    Declare PromptOptionPicker node

--- __init__.py
+++ __init__.py
@@ -1,5 +1,11 @@
 """Top-level package for the node pack."""
 
+from .src.nodes.prompt_option_picker import (
+    NODE_CLASS_MAPPINGS as PromptOptionPickerMappings,
+)
+from .src.nodes.prompt_option_picker import (
+    NODE_DISPLAY_NAME_MAPPINGS as PromptOptionPickerDisplayMappings,
+)
 from .src.nodes.prompt_preset_selector import (
     NODE_CLASS_MAPPINGS as PromptPresetSelectorMappings,
 )
@@ -9,9 +15,11 @@ from .src.nodes.prompt_preset_selector import (
 
 NODE_CLASS_MAPPINGS = {}
 NODE_CLASS_MAPPINGS.update(PromptPresetSelectorMappings)
+NODE_CLASS_MAPPINGS.update(PromptOptionPickerMappings)
 
 NODE_DISPLAY_NAME_MAPPINGS = {}
 NODE_DISPLAY_NAME_MAPPINGS.update(PromptPresetSelectorDisplayMappings)
+NODE_DISPLAY_NAME_MAPPINGS.update(PromptOptionPickerDisplayMappings)
 
 # Node registration
 __all__ = [
