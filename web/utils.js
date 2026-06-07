e8b28e30445593d31ec52c6692000417995892a7
relhamdi <elhamdi.rayan@gmail.com>
2026-06-08 00:23:36 +0200

    Use findWidget in flashButton

--- web/utils.js
+++ web/utils.js
@@ -396,9 +396,7 @@ export const waitForWidgets = (node, callback) => {
 // --- Buttons ---
 
 export const flashButton = (node, index, flashLabel) => {
-    const buttonRowWidget = node.widgets.find(
-        (w) => w.name === "action_buttons",
-    );
+    const buttonRowWidget = findWidget(node, "action_buttons");
     if (!buttonRowWidget) return;
     const btn = buttonRowWidget.buttons[index];
 
@@ -407,7 +405,6 @@ export const flashButton = (node, index, flashLabel) => {
 
     // Save original if not flashing
     if (!btn._originalLabel) btn._originalLabel = btn.label;
-
     btn.label = flashLabel;
     if (node.graph) node.graph.setDirtyCanvas(true, true);
 
