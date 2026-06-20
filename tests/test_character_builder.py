3c36162b7f5ba6f4014c9a7362bb368fe2bfe7ae
relhamdi <elhamdi.rayan@gmail.com>
2026-06-20 20:45:57 +0200

    Update tests

--- tests/test_character_builder.py
+++ tests/test_character_builder.py
@@ -14,16 +14,11 @@ def run(node, **kwargs):
         show_eyeballs=True,
         eyes_override="",
         eyes_override_mode=False,
-        pupils_override="",
-        pupils_override_mode=False,
         eyewear_override="",
         eyewear_override_mode=False,
-        teeth_override="",
-        teeth_override_mode=False,
         face_details_override="",
         face_details_override_mode=False,
-        hair_style_override_options="",
-        hair_style_override_selected="",
+        bald=False,
         show_piercings=True,
         face_piercings_override="",
         face_piercings_override_mode=False,
@@ -90,12 +85,12 @@ class TestCharacterBuilderHead:
     # --- Eyes group ---
 
     def test_eye_color_and_type_in_head(self, node):
-        head, _, _ = run(node, eye_color="blue eyes", eye_type="almond eyes")
+        head, _, _, _, _ = run(node, eye_color="blue eyes", eye_type="almond eyes")
         assert "blue eyes" in head
         assert "almond eyes" in head
 
     def test_eyes_override_replace_mode(self, node):
-        head, _, _ = run(
+        head, _, _, _, _ = run(
             node,
             eye_color="blue eyes",
             eye_type="almond eyes",
@@ -107,7 +102,7 @@ class TestCharacterBuilderHead:
         assert "almond eyes" not in head
 
     def test_eyes_override_add_mode(self, node):
-        head, _, _ = run(
+        head, _, _, _, _ = run(
             node,
             eye_color="blue eyes",
             eyes_override="sparkle",
@@ -117,37 +112,31 @@ class TestCharacterBuilderHead:
         assert "sparkle" in head
 
     def test_eyes_excluded_when_show_eyes_false(self, node):
-        head, _, _ = run(node, eye_color="blue eyes", show_eyes=False)
+        head, _, _, _, _ = run(node, eye_color="blue eyes", show_eyes=False)
         assert "blue eyes" not in head
 
     def test_eyes_excluded_when_show_eyeballs_false(self, node):
-        head, _, _ = run(node, eye_color="blue eyes", show_eyeballs=False)
+        head, _, _, _, _ = run(node, eye_color="blue eyes", show_eyeballs=False)
         assert "blue eyes" not in head
 
-    def test_pupils_override_replace(self, node):
-        head, _, _ = run(
-            node,
-            pupils="round pupils",
-            pupils_override="heart pupils",
-            pupils_override_mode=True,
-        )
-        assert "heart pupils" in head
-        assert "round pupils" not in head
+    def test_pupils_included(self, node):
+        head, _, _, _, _ = run(node, pupils="round pupils")
+        assert "round pupils" in head
 
     def test_eye_details_included_when_show_eyes(self, node):
-        head, _, _ = run(node, eye_details="sparkly", show_eyes=True)
+        head, _, _, _, _ = run(node, eye_details="sparkly", show_eyes=True)
         assert "sparkly" in head
 
     def test_eye_details_excluded_when_show_eyes_false(self, node):
-        head, _, _ = run(node, eye_details="sparkly", show_eyes=False)
+        head, _, _, _, _ = run(node, eye_details="sparkly", show_eyes=False)
         assert "sparkly" not in head
 
     def test_eye_effects_always_included(self, node):
-        head, _, _ = run(node, eye_effects="glowing", show_eyes=False)
+        head, _, _, _, _ = run(node, eye_effects="glowing", show_eyes=False)
         assert "glowing" in head
 
     def test_eyewear_override_replace(self, node):
-        head, _, _ = run(
+        head, _, _, _, _ = run(
             node,
             eyewear="glasses",
             eyewear_override="round eyewear",
@@ -157,40 +146,41 @@ class TestCharacterBuilderHead:
         assert "glasses" not in head
 
     def test_mouth_type_included(self, node):
-        head, _, _ = run(node, mouth_type="small mouth")
+        head, _, _, _, _ = run(node, mouth_type="small mouth")
         assert "small mouth" in head
 
-    def test_teeth_override_replace(self, node):
-        head, _, _ = run(
-            node,
-            teeth="normal teeth",
-            teeth_override="sharp teeth",
-            teeth_override_mode=True,
-        )
+    def test_teeth_included(self, node):
+        head, _, _, _, _ = run(node, teeth="sharp teeth")
         assert "sharp teeth" in head
-        assert "normal teeth" not in head
 
     # --- Hair group ---
 
     def test_hair_color_included(self, node):
-        head, _, _ = run(node, hair_color="black hair")
+        head, _, _, _, _ = run(node, hair_color="black hair")
         assert "black hair" in head
 
-    def test_hair_style_override_takes_priority(self, node):
-        head, _, _ = run(
-            node, hair_style="ponytail", hair_style_override_selected="braids"
+    def test_bald_outputs_bald_text(self, node):
+        head, _, _, _, _ = run(node, bald=True)
+        assert "bald" in head
+
+    def test_bald_excludes_hair_color_and_style(self, node):
+        head, _, _, _, _ = run(
+            node, bald=True, hair_color="black hair", hair_style="ponytail"
         )
-        assert "braids" in head
+        assert "black hair" not in head
         assert "ponytail" not in head
 
-    def test_hair_style_used_when_no_override(self, node):
-        head, _, _ = run(node, hair_style="ponytail", hair_style_override_selected="")
+    def test_hair_color_and_style_used_when_not_bald(self, node):
+        head, _, _, _, _ = run(
+            node, bald=False, hair_color="black hair", hair_style="ponytail"
+        )
+        assert "black hair" in head
         assert "ponytail" in head
 
     # --- Face / makeup group ---
 
     def test_face_details_override_add(self, node):
-        head, _, _ = run(
+        head, _, _, _, _ = run(
             node,
             face_details="freckles",
             face_details_override="scar",
@@ -200,15 +190,15 @@ class TestCharacterBuilderHead:
         assert "scar" in head
 
     def test_face_piercings_included_when_show_piercings(self, node):
-        head, _, _ = run(node, face_piercings="nose ring", show_piercings=True)
+        head, _, _, _, _ = run(node, face_piercings="nose ring", show_piercings=True)
         assert "nose ring" in head
 
     def test_face_piercings_excluded_when_show_piercings_false(self, node):
-        head, _, _ = run(node, face_piercings="nose ring", show_piercings=False)
+        head, _, _, _, _ = run(node, face_piercings="nose ring", show_piercings=False)
         assert "nose ring" not in head
 
     def test_nail_color_override_replace(self, node):
-        head, _, _ = run(
+        head, _, _, _, _ = run(
             node,
             nail_color="red nails",
             nail_color_override="blue nails",
@@ -220,116 +210,117 @@ class TestCharacterBuilderHead:
         assert "red nails" not in head
 
     def test_nail_color_excluded_when_show_nails_false(self, node):
-        head, _, _ = run(node, nail_color="red nails", show_nails=False)
+        head, _, _, _, _ = run(node, nail_color="red nails", show_nails=False)
         assert "red nails" not in head
 
     def test_nail_color_excluded_when_show_makeup_false(self, node):
-        head, _, _ = run(
+        head, _, _, _, _ = run(
             node, nail_color="red nails", show_nails=True, show_makeup=False
         )
         assert "red nails" not in head
 
     def test_nail_type_included_when_show_nails(self, node):
-        head, _, _ = run(node, nail_type="long nails", show_nails=True)
+        head, _, _, _, _ = run(node, nail_type="long nails", show_nails=True)
         assert "long nails" in head
 
     def test_nail_type_excluded_when_show_nails_false(self, node):
-        head, _, _ = run(node, nail_type="long nails", show_nails=False)
+        head, _, _, _, _ = run(node, nail_type="long nails", show_nails=False)
         assert "long nails" not in head
 
     def test_makeup_included_when_show_makeup(self, node):
-        head, _, _ = run(node, makeup="red lipstick", show_makeup=True)
+        head, _, _, _, _ = run(node, makeup="red lipstick", show_makeup=True)
         assert "red lipstick" in head
 
     def test_makeup_excluded_when_show_makeup_false(self, node):
-        head, _, _ = run(node, makeup="red lipstick", show_makeup=False)
+        head, _, _, _, _ = run(node, makeup="red lipstick", show_makeup=False)
         assert "red lipstick" not in head
 
     def test_makeup_modifiers_included_when_show_makeup(self, node):
-        head, _, _ = run(node, makeup_modifiers="glossy", show_makeup=True)
+        head, _, _, _, _ = run(node, makeup_modifiers="glossy", show_makeup=True)
         assert "glossy" in head
 
     # --- General head ---
 
     def test_head_in_parentheses_groups(self, node):
-        head, _, _ = run(
+        head, _, _, _, _ = run(
             node, eye_color="blue eyes", hair_color="black hair", makeup="lipstick"
         )
         assert "(" in head and ")" in head
 
     def test_head_all_empty_returns_empty(self, node):
-        head, _, _ = run(node)
+        head, _, _, _, _ = run(node)
         assert head == ""
 
     def test_head_cleanup_applied(self, node):
-        head, _, _ = run(node, eye_color="blue eyes,")
+        head, _, _, _, _ = run(node, eye_color="blue eyes,")
         assert not head.endswith(",")
 
 
 class TestCharacterBuilderAccessories:
     def test_facewear_override_replace(self, node):
-        _, acc, _ = run(
+        _, _, facewear, _, _ = run(
             node,
             facewear="sticker",
             facewear_override="tattoo",
             facewear_override_mode=True,
         )
-        assert "tattoo" in acc
-        assert "sticker" not in acc
+        assert "tattoo" in facewear
+        assert "sticker" not in facewear
 
     def test_neckwear_included(self, node):
-        _, acc, _ = run(node, neckwear="choker")
-        assert "choker" in acc
+        _, _, _, neckwear, _ = run(node, neckwear="choker")
+        assert "choker" in neckwear
 
     def test_armwear_included(self, node):
-        _, acc, _ = run(node, armwear="ring")
-        assert "ring" in acc
+        _, _, _, _, armwear = run(node, armwear="ring")
+        assert "ring" in armwear
 
     def test_accessories_excluded_when_toggle_false(self, node):
-        _, acc, _ = run(
+        _, _, facewear, neckwear, armwear = run(
             node,
             facewear="sticker",
             neckwear="choker",
+            armwear="ring",
             toggle_accessories=False,
         )
-        assert acc == ""
+        assert facewear == ""
+        assert neckwear == ""
+        assert armwear == ""
 
     def test_accessories_all_empty_returns_empty(self, node):
-        _, acc, _ = run(node)
-        assert acc == ""
-
-    def test_accessories_in_parentheses(self, node):
-        _, acc, _ = run(node, neckwear="choker")
-        assert acc == "(choker)"
+        _, _, facewear, neckwear, armwear = run(node)
+        assert facewear == ""
+        assert neckwear == ""
+        assert armwear == ""
 
 
 class TestCharacterBuilderBody:
     def test_base_body_included(self, node):
-        _, _, body = run(node, base_body="slim")
+        _, body, _, _, _ = run(node, base_body="slim")
         assert "slim" in body
 
     def test_body_type_included(self, node):
-        _, _, body = run(node, body_type="athletic")
+        _, body, _, _, _ = run(node, body_type="athletic")
         assert "athletic" in body
 
     def test_skin_color_included(self, node):
-        _, _, body = run(node, skin_color="tan")
+        _, body, _, _, _ = run(node, skin_color="tan")
         assert "tan" in body
 
     def test_body_details_included(self, node):
-        _, _, body = run(node, body_details="scar")
+        _, body, _, _, _ = run(node, body_details="scar")
         assert "scar" in body
 
     def test_upper_body_included_when_show_upper_body(self, node):
-        _, _, body = run(node, upper_body="pectorals", show_upper_body=True)
+        _, body, _, _, _ = run(node, upper_body="pectorals", show_upper_body=True)
         assert "pectorals" in body
 
     def test_upper_body_excluded_when_show_upper_body_false(self, node):
-        _, _, body = run(node, upper_body="pectorals", show_upper_body=False)
+        _, body, _, _, _ = run(node, upper_body="pectorals", show_upper_body=False)
         assert "pectorals" not in body
 
     def test_upper_piercings_override_replace(self, node):
-        _, _, body = run(
+        _, body, _, _, _ = run(
             node,
             upper_piercings="cross piercing",
             upper_piercings_override="other ring",
@@ -341,7 +332,7 @@ class TestCharacterBuilderBody:
         assert "cross piercing" not in body
 
     def test_upper_piercings_excluded_when_show_piercings_false(self, node):
-        _, _, body = run(
+        _, body, _, _, _ = run(
             node,
             upper_piercings="cross piercing",
             show_upper_body=True,
@@ -350,15 +341,15 @@ class TestCharacterBuilderBody:
         assert "cross piercing" not in body
 
     def test_mid_body_included_when_show_mid_body(self, node):
-        _, _, body = run(node, mid_body="abs", show_mid_body=True)
+        _, body, _, _, _ = run(node, mid_body="abs", show_mid_body=True)
         assert "abs" in body
 
     def test_mid_body_excluded_when_show_mid_body_false(self, node):
-        _, _, body = run(node, mid_body="abs", show_mid_body=False)
+        _, body, _, _, _ = run(node, mid_body="abs", show_mid_body=False)
         assert "abs" not in body
 
     def test_mid_piercings_override_replace(self, node):
-        _, _, body = run(
+        _, body, _, _, _ = run(
             node,
             mid_piercings="navel piercing",
             mid_piercings_override="other ring",
@@ -370,15 +361,15 @@ class TestCharacterBuilderBody:
         assert "navel piercing" not in body
 
     def test_lower_body_included_when_show_lower_body(self, node):
-        _, _, body = run(node, lower_body="wide hips", show_lower_body=True)
+        _, body, _, _, _ = run(node, lower_body="wide hips", show_lower_body=True)
         assert "wide hips" in body
 
     def test_lower_body_excluded_when_show_lower_body_false(self, node):
-        _, _, body = run(node, lower_body="wide hips", show_lower_body=False)
+        _, body, _, _, _ = run(node, lower_body="wide hips", show_lower_body=False)
         assert "wide hips" not in body
 
     def test_lower_piercings_override_replace(self, node):
-        _, _, body = run(
+        _, body, _, _, _ = run(
             node,
             lower_piercings="hip ring",
             lower_piercings_override="other ring",
@@ -390,19 +381,19 @@ class TestCharacterBuilderBody:
         assert "hip ring" not in body
 
     def test_butt_included_when_show_butt(self, node):
-        _, _, body = run(node, butt="butt", show_butt=True)
+        _, body, _, _, _ = run(node, butt="butt", show_butt=True)
         assert "butt" in body
 
     def test_butt_excluded_when_show_butt_false(self, node):
-        _, _, body = run(node, butt="butt", show_butt=False)
+        _, body, _, _, _ = run(node, butt="butt", show_butt=False)
         assert "butt" not in body
 
     def test_body_modifiers_included(self, node):
-        _, _, body = run(node, body_modifiers="toned")
+        _, body, _, _, _ = run(node, body_modifiers="toned")
         assert "toned" in body
 
     def test_show_body_false_excludes_everything(self, node):
-        _, _, body = run(
+        _, body, _, _, _ = run(
             node,
             base_body="slim",
             body_type="athletic",
@@ -417,13 +408,13 @@ class TestCharacterBuilderBody:
         assert body == ""
 
     def test_body_all_empty_returns_empty(self, node):
-        _, _, body = run(node)
+        _, body, _, _, _ = run(node)
         assert body == ""
 
     def test_body_in_parentheses(self, node):
-        _, _, body = run(node, base_body="slim")
+        _, body, _, _, _ = run(node, base_body="slim")
         assert body == "(slim)"
 
     def test_body_cleanup_applied(self, node):
-        _, _, body = run(node, base_body="slim,")
+        _, body, _, _, _ = run(node, base_body="slim,")
         assert not body.endswith(",)")
